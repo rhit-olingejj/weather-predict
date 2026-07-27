9d7dad0e78a1f00049ccc5260f5b26d50143c86b
Alex <ryzen71511@outlook.com>
2026-07-26 17:39:03 -0700

    created a test train validation split script from already made X and y. I randomly pick data for the 6 output dataframes.

--- test_train_validate_test_split.py
+++ test_train_validate_test_split.py
@@ -1,12 +1,16 @@
 import pytest
-import Train_validate_test_split from ./train_validate_test_split.py
+import pandas as pd
+import numpy as np
+from train_validate_test_split import Train_validate_test_split
 
 def test_split_sizes():
     X = {
-        col1: [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68]
-        col2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+        "col1": np.zeros(400),
+        "col2": np.zeros(400)
     }
-    X_size = X.size
+
+    X_df = pd.DataFrame.from_dict(X)
+    X_size = X_df.size
     X_train_validate_size = X_size * 0.8
     X_test_size = X_size * 0.2
 
@@ -14,9 +18,10 @@ def test_split_sizes():
     X_validate_size = X_train_validate_size * 0.2
 
     y = {
-        col3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+        "col3": np.zeros(400)
     }
-    y_size = y.size
+    y_df = pd.DataFrame.from_dict(y)
+    y_size = y_df.size
     
     y_train_validate_size = y_size * 0.8
     y_test_size = y_size * 0.2
@@ -24,7 +29,7 @@ def test_split_sizes():
     y_train_size = y_train_validate_size * 0.8
     y_validate_size = y_train_validate_size * 0.2
 
-    X_train, X_validate, X_test, y_train, y_validate, y_test = Train_validate_test_split(X, y)
+    X_train, X_validate, X_test, y_train, y_validate, y_test = Train_validate_test_split(X_df, y_df)
     
     assert X_train.size == X_train_size
     assert y_train.size == y_train_size
--- train_validate_test_split.py
+++ train_validate_test_split.py
@@ -5,23 +5,12 @@ import pandas as pd
 def Train_validate_test_split(X, y):
     dataframe_size = X.size # since X and y are from the same dataframe, the size is the same
 
-    test_df_size = dataframe_size * 0.2
-    validation_df_size = test_df_size * 0.3
-
+    test_df_size = 0.2 # test data splits off from train + validation dataset, intial df for X_train and y_train
+    train_df_size = 0.8
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_df_size, random_state=42)
-    X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, test_size = validation_df_size, random_size=42)
+    print(X_train.size)
 
-    return X_train, X_validate, x_test, y_train, y_validate, y_test
+    validation_df_size = 0.2 # next split from train
+    X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, test_size = validation_df_size, random_state=42) # second values for X_train and y_train with only training data
 
-
-train_validate_df_size = dataframe_size * 0.8
-X_train_validate = X.iloc[:train_validate_df_size]
-X_test = X.iloc[train_validate_df_size:]
-y_train_validate = y.iloc[:train_validate_df_size]
-y_test = y.iloc[train_validate_df_size:]
-
-train_df_size = train_validate_df_size * 0.8
-X_train = X.iloc[:train_df_size]
-X_validate = X.iloc[train_df_size:]
-y_train = y.iloc[:train_df_size]
-y_validate = y.iloc[train_df_size:]
+    return X_train, X_validate, X_test, y_train, y_validate, y_test
